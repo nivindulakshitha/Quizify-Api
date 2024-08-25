90faabe87a30b4b7a5a4fd85767a10f6b8964d43
nivindulakshitha <nivindulakshitha@gmail.com>
2024-08-25 19:02:27 +0530

    chore: Add user login route to UserRoutes

--- dist/Routes/UserRoutes.js
+++ dist/Routes/UserRoutes.js
@@ -5,10 +5,12 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 var _express = require("express");
+var _UserLogin = _interopRequireDefault(require("../Methods/UserLogin.js"));
 var _UserRegistration = _interopRequireDefault(require("../Methods/UserRegistration.js"));
 var _UserForgotPassword = _interopRequireDefault(require("../Methods/UserForgotPassword.js"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 var userRouter = (0, _express.Router)();
+userRouter.post("/login", _UserLogin["default"]);
 userRouter.post("/register", _UserRegistration["default"]);
 userRouter.post("/forgot-password", _UserForgotPassword["default"]);
 var _default = exports["default"] = userRouter;
